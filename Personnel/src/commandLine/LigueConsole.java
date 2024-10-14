23e99bc0f75616ffc886d7f43368359aba296c85
EL ARCHE Wassim <welarche>
2024-10-14 17:29:18 +0200

    oublie de remplissage des attribut date dans l'appel du constructeur dans d'autre classe

--- Personnel/src/commandLine/LigueConsole.java
+++ Personnel/src/commandLine/LigueConsole.java
@@ -99,7 +99,7 @@ public class LigueConsole
 				{
 					ligue.addEmploye(getString("nom : "), 
 						getString("prenom : "), getString("mail : "), 
-						getString("password : "));
+						getString("password : ") , getString("date arrive : ") , getString("date depart : "));
 				}
 		);
 	}
--- Personnel/src/personnel/Ligue.java
+++ Personnel/src/personnel/Ligue.java
@@ -108,9 +108,9 @@ public class Ligue implements Serializable, Comparable<Ligue>
 	 * @return l'employé créé. 
 	 */
 
-	public Employe addEmploye(String nom, String prenom, String mail, String password)
+	public Employe addEmploye(String nom, String prenom, String mail, String password, LocalDate date_arrivee , LocalDate date_depart)
 	{
-		Employe employe = new Employe(this.gestionPersonnel, this, nom, prenom, mail, password);
+		Employe employe = new Employe(this.gestionPersonnel, this, nom, prenom, mail, password, date_arrivee , date_depart);
 		employes.add(employe);
 		return employe;
 	}
